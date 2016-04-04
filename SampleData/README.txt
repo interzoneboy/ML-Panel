6bb4e2a123e5356846650f3e42e6900e9be955e2
Matt <mattthew@gmail.com>
2016-04-04 16:35:24 -0400

    Add data handling code and example data

---
+++ SampleData/README.txt
@@ -0,0 +1,9 @@
+
+
+These are example x and y variables from an anonymized dataset. The x variables are DXA measurements
+(as raw as we get them... unsure of any upstream processing), and the y variables are MRI
+measurements. As example tasks, predict any/all of the y variables using the x ones. The varibles
+named *_agg are aggregate variables produced from the * variables (eg) means, sums, ratios), so perhaps
+they shouldn't be included in models directly with their constituents, or things might get confusing.
+
+
---
+++ dataset.py
@@ -0,0 +1,73 @@
+"""
+
+Functions to handle all the data fetching/cleaning/normalizing/splitting/etc.
+These will produce a uniform tuple output for the algorithms to make use of.
+All messy data details are sealed off from the algorithms, pipelining, and 
+visualization code, in this module.
+
+"""
+from scipy import linalg
+import numpy as np
+import copy
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib import colors
+import pandas as pd
+from sklearn.cross_validation import ShuffleSplit
+from sklearn import preprocessing
+from IPython.terminal.embed import InteractiveShellEmbed
+ipshell = InteractiveShellEmbed(banner1="dataset breakpoint")
+
+
+
+def read_data_csv(filename, *args, **kwargs):
+    """ Read <filename> and return a pandas data frame.
+
+    :param filename: Filename to read.
+    :type filename: str
+    """
+    return pd.read_csv(filename)
+
+
+def randomSplit(df, y_var_name, x_var_names, testSize=0.35, seedIn=None):
+    """ Scale, impute missing, then split dataset *df* randomly into tuple (test_x, test_y, train_x, train_y)
+
+    :param df: the pandas data frame to split
+    :param y_var_name: the name of the variable (column of df) we try to predict.
+    :type y_var_name: str
+    :param x_var_names: list of the predictor variables.
+    :type x_var_names: list(str)
+    :param testSize: the fraction (0.0 to 1.0) of the dataset to put in TEST partition.
+    :param seedIn: seed to the random number generator for reproducible results (what ARE those even).
+    :returns: dict -- {'train_x','train_y','test_x','test_y'}
+    """
+    scaler = preprocessing.StandardScaler()
+    imputer = preprocessing.Imputer(missing_values="NaN", strategy="mean", axis=0)
+
+    # remove rows where y_variable is missing.
+    good_inds = (np.isnan(df[y_var_name])==False).nonzero()
+    d = df.iloc[good_inds]
+
+    if seedIn!=None:
+        ss = ShuffleSplit(d.shape[0], n_iter=1, test_size=testSize, random_state=seedIn)
+    else:
+        ss = ShuffleSplit(d.shape[0], n_iter=1, test_size=testSize)
+
+    training_inds, test_inds = ss.__iter__().next()
+    training_rows = d.iloc[training_inds]
+    test_rows = d.iloc[test_inds]
+
+    data_tr = training_rows[x_var_names]
+    imputer.fit(data_tr)
+    scaler.fit(imputer.transform(data_tr))
+    data_tr_scaled = scaler.transform(imputer.transform(data_tr))
+
+    data_test = test_rows[x_var_names]
+    data_test_scaled = scaler.transform(imputer.transform(data_test))
+
+    return {'test_x': data_test_scaled,
+            'test_y': test_rows[y_var_name],
+            'train_x': data_tr_scaled,
+            'train_y': training_rows[y_var_name]
+            }
+

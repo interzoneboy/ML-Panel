6718585ecc3dab583df3d19f452531a1b5358516
Matt <mattthew@gmail.com>
2016-04-21 15:20:18 -0400

    Hoist testing code from __main__ into separate method

--- pipeline.py
+++ pipeline.py
@@ -31,15 +31,45 @@ def stdout_redirected(new_stdout):
 import dataset as DS
 import algorithm_classification as ALG
 
-
-if __name__ == "__main__":
-
+def run_test():
     d = DS.read_data_csv("SampleData/example_data.csv")
     d2 = DS.compute_quantiles(d, "y_var_3", 4)
     d3 = DS.top_or_bottom(d2, "qtile_4_y_var_3", collapse=True)
-
+    
     x_names = [a for a in d3.columns if ('x_var' in a.lower() and ('agg' not in a.lower()))]
     y_name = "TBqtile_4_y_var_3"
-
+    
     dReady = DS.randomSplit(d3, y_name, x_names)
+    
+    alg_results = {}
+    for kk in ['tree']:
+        print "Working on " + str(kk)
+        alg_results[kk] = []
+        for numIter in range(0, 500):
+            alg_results[kk].append(ALG.algorithms[kk](dReady))
+            if numIter%20==0:
+                print "Finished %s iterations." % (str(numIter),)
 
+    return alg_results
+
+
+if __name__ == "__main__":
+
+    #res = run_test()
+    d = DS.read_data_csv("SampleData/example_data.csv")
+    d2 = DS.compute_quantiles(d, "y_var_3", 4)
+    d3 = DS.top_or_bottom(d2, "qtile_4_y_var_3", collapse=True)
+    
+    x_names = [a for a in d3.columns if ('x_var' in a.lower() and ('agg' not in a.lower()))]
+    y_name = "TBqtile_4_y_var_3"
+    
+    dReady = DS.randomSplit(d3, y_name, x_names)
+    
+    alg_results = {}
+    for kk in ['svm_linear','svm_rbf','lda','qda','logistic','SGDlogistic']:
+        print "Working on " + str(kk)
+        alg_results[kk] = []
+        for numIter in range(0, 100):
+            alg_results[kk].append(ALG.algorithms[kk](dReady))
+            if numIter%20==0:
+                print "Finished %s iterations." % (str(numIter),)
